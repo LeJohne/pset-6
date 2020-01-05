c58d391dbfa2e42dde8f4ec9f47247fe4602b426
LeJohne <johngoy11024@gmail.com>
2020-01-05 15:50:05 -0500

    commit 6 - worked on js

--- js/app.js
+++ js/app.js
@@ -2,30 +2,78 @@ const clear = document.querySelector(".clear");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
-const check = "fa-check-circle";
-const uncheck = "fa-check-thin"
-const linethrough = "lineThrough"
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
 
-function addToDo(toDo){
-  const text = '
-                <i class="fa fa-circle thin co" job="complete" id= "0"></i>
-                <p class="text">${toDo}</p>
-                <i class="fa fa-trash-o delete" job="delete" id="0"</i>
-               ';
+let LIST = []
+    , id = 0;
+
+//To Do Function
+
+function addToDo(toDo, id, done, trash){
+
+  if(trash){ return; }
+
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : "";
+
+  const item = `<li class="item">
+                <i class="fa ${DONE} co" job="complete" id= "${id}"></i>
+                <p class="text ${LINE}">${toDo}</p>
+                <i class="fa fa-trash-o delete" job="delete" id="${id}"</i>
+                </li>`;
   const position = "beforeend";
 
-  list.insertAdjacentHTML(position, text);
+  list.insertAdjacentHTML(position, item);
 }
-addToDo("drink coffee")
 
-
-document.addEventListener("keyup", function(event){
+// Adding item by pressing enter key
+document.addEventListener("keyup", function(even){
   if(event.keyCode == 13){
     const toDo = input.value;
+
+    //Checks input isn't empty
     if (toDo) {
       addToDo(toDo);
-    }
-    input.value = ""
-  }
 
+      LIST.push({
+        name : toDo?
+        id : id,
+        done : false,
+        trash : false
+      });
+
+      id++;
+    }
+    input.value = "";
+  }
+});
+
+//Complete Function
+function completeToDo(element){
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+  LIST[element.id].done = LIST [element.id].done ? false : true;
+}
+
+//Remove Function
+function removeToDo(element){
+  element.parentNode.parentNode.removeChild(element.parentNode);
+
+  LIST[element.id].trash = true;
+}
+
+//Target function
+
+list.addEventListener("click", function(event){
+  const element = even.target;
+  const elementJob = element.attributes.job.value;
+  if(elementJob == "complete"){
+    completeToDo(element);
+  }else if(elementJob == "remove"){
+    removeToDo(element);
+  }
 });
